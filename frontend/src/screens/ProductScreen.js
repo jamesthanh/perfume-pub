519677dffb75c828cc3a1aad8265f2d9c6890a8c
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-06 16:50:40 +0700

    finish building first phrase of the product listing

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
 
-const ProductScreen = () => {
-  return <div>Product</div>;
+  return (
+    <>
+      <Link className='btn btn-primary my-3' to='/'>
+        Về trang chủ
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} lượt`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Thông tin sản phẩm</strong>{' '}
+            </ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    <h8>Giá:</h8>
+                  </Col>
+                  <Col>
+                    <h8>{product.price} VND</h8>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    <h8>Tình trạng:</h8>
+                  </Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'Còn hàng' : 'Đã bán hết'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Thêm vào giỏ
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
