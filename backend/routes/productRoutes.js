bee6d913584c6ad1c61fdcb7c46602b1b23116b3
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-07 16:52:16 +0700

    add fetching all products and get product by id from db

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import aysncHandler from 'express-async-handler';
+const router = express.Router();
+
+import Product from '../models/productModel.js';
+
+// @desc Fetch all product
+// @route GET /api/product
+// @access Public
+router.get(
+  '/',
+  aysncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// @desc Fetch single prodict
+// @route GET /api/product/:id
+// @access Public
+router.get(
+  '/:id',
+  aysncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Không tìm thấy sản phẩm.' });
+    }
+  })
+);
+
+export default router;
