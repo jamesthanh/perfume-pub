aa35510b19f5605e4626a3ccf7bbef84057013b6
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-08 17:08:46 +0700

    Add spinner and formatting messages

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,8 +2,12 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
-export const productListReducers = (
+
+export const productListReducer = (
   state = {
     products: [],
   },
@@ -18,6 +22,30 @@ export const productListReducers = (
         products: action.payload,
       };
     case PRODUCT_LIST_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = {
+    product: { reviews: [] },
+  },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state };
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload,
+      };
+    case PRODUCT_DETAILS_FAIL:
       return {
         loading: false,
         error: action.payload,
