fdc250de7ed79bd2b8ae059c8fc66bdf551f4be0
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-09 14:53:01 +0700

    add cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -28,7 +28,97 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, qty]);
 
-  return <div>Cart</div>;
+  const removeFromCartHandler = (id) => {
+    console.log('removed');
+  };
+
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping');
+  };
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Giỏ Hàng</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Bạn chưa chọn sản phẩm nào.{' '}
+            <Link to='/'>
+              <strong>Mua ngay!</strong>
+            </Link>{' '}
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>
+                    <p>{item.price} VND</p>
+                  </Col>
+                  <Col>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card variant='flush'>
+          <ListGroup.Item>
+            <h2>
+              Đơn tiền của ({cartItems.reduce((acc, item) => acc + item.qty, 0)}
+              ) sản phẩm
+            </h2>
+            {cartItems
+              .reduce((acc, item) => acc + item.qty * item.price, 0)
+              .toFixed(2)}{' '}
+            VND
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Button
+              type='button'
+              className='btn-block'
+              disabled={cartItems.length === 0}
+              onClick={checkoutHandler}
+            >
+              Thanh toán
+            </Button>
+          </ListGroup.Item>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartScreen;
