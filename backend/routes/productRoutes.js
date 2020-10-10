d1c938b077931720352075fe836bd17170f7e293
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-10 11:43:29 +0700

    add product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,11 @@
 import express from 'express';
-import aysncHandler from 'express-async-handler';
 const router = express.Router();
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js';
 
-import Product from '../models/productModel.js';
-
-// @desc Fetch all product
-// @route GET /api/product
-// @access Public
-router.get(
-  '/',
-  aysncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-// @desc Fetch single prodict
-// @route GET /api/product/:id
-// @access Public
-router.get(
-  '/:id',
-  aysncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Không tìm thấy sản phẩm');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
