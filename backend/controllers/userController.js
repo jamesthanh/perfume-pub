792d0879655d778776764bef0b0cc496f3aa7dcc
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-10 14:05:54 +0700

    add user register function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,37 @@ const authUser = aysncHandler(async (req, res) => {
   }
 });
 
+// @desc Register a new user
+// @route GET /api/users
+// @access Public
+const registerUser = aysncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error('user already exists');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
 // @desc get user profile
 // @route POST /api/users/profile
 // @access Private
@@ -41,4 +72,4 @@ const getUserProfile = aysncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
