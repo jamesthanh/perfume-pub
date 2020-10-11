3ef6983a315791fc37caf32783ff938e99572b9e
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-11 13:52:27 +0700

    add login screen with functionalities

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import FormController from '../components/FormController';
+import { login } from '../actions/userActions';
+
+const LoginScreen = ({ location, history }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  return (
+    <FormController>
+      <h1>Đăng ký</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Nhập Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='password'>
+          <Form.Label>Mật Khẩu</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Nhập Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type='submit' variant='primary'>
+          Đăng nhập
+        </Button>
+      </Form>
+      <Row className='py-3'>
+        <Col>
+          Khách hàng mới?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Đăng ký ngay
+          </Link>
+        </Col>
+      </Row>
+    </FormController>
+  );
+};
+
+export default LoginScreen;
