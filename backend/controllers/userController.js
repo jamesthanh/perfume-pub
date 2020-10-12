cf716cb4f97d9268215ceb3d9fa9ec5fa9c6e574
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-12 17:33:34 +0700

    add update user profile endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,4 +72,31 @@ const getUserProfile = aysncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+// @desc update user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = aysncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
