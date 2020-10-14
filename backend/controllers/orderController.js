36fedb48f0205a2b062155c97d30f0967e9748fc
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-14 16:42:35 +0700

    create order route controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+import aysncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+
+// @desc Create new order all
+// @route GET /api/orders
+// @access Private
+const addOrderItems = aysncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
+});
+
+export { addOrderItems };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,7 +8,7 @@ import {
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
-router.route('').post(registerUser);
+router.route('/').post(registerUser);
 router.post('/login', authUser);
 router
   .route('/profile')
