c5ab98067b89a79351a25e14d2784aef32f23e34
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-19 12:27:32 +0700

    add get all orders for admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -83,4 +83,19 @@ const getMyOrders = aysncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id });
   res.json(orders);
 });
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+
+// @desc Get add orders
+// @route GET /api/orders/myorders
+// @access Private/Admin
+const getOrders = aysncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.json(orders);
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+};
--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
   filename(req, file, cb) {
     cb(
       null,
-      `${file.filename}-${Date.now()}${path.extname(file.originalName)}`
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
     );
   },
 });
@@ -19,10 +19,11 @@ function checkFileType(file, cb) {
   const filetypes = /jpg|jpeg|png/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
+
   if (extname && mimetype) {
     return cb(null, true);
   } else {
-    cb('Chỉ cho phép định dạng ảnh');
+    cb('Images only!');
   }
 }
 
@@ -33,7 +34,7 @@ const upload = multer({
   },
 });
 
-router.post('/', upload.single('image'), (res, req) => {
+router.post('/', upload.single('image'), (req, res) => {
   res.send(`/${req.file.path}`);
 });
 
