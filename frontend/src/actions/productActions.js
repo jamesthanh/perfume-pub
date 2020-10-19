2ff98417f53f99ffa5a9ef1952b6c1aa1353da8a
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-19 16:40:33 +0700

    create search box component

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -22,11 +22,11 @@ import {
 
 import { logout } from '../actions/userActions';
 
-export const listProducts = () => async (dispatch) => {
+export const listProducts = (keyword = '') => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get('/api/products');
+    const { data } = await axios.get(`/api/products?keyword=${keyword}`);
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
       payload: data,
