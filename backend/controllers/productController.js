f7c49cc10224998579ea483010f713569d841bfc
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-10-20 15:52:43 +0700

    add top product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -143,6 +143,14 @@ const createProductReview = aysncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get top rated product
+// @route   GET /api/products/top
+// @access  Public
+const getTopProduct = aysncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(5);
+  res.json(products);
+});
+
 export {
   getProductById,
   getProducts,
@@ -150,4 +158,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProduct,
 };
